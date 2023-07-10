6f0ab2206406b3df456d8622aea81d3446cbdfa3
Andrew Muth <agmuth@gmail.com>
2023-07-10 14:16:11 -0600

    commented utils

--- linprog/utils.py
+++ linprog/utils.py
@@ -1,10 +1,12 @@
 import numpy as np 
 
 primal_simplex_div = np.vectorize(
+    # Special division option used in primal algorithm.
     lambda n, d: n / d if d > 0 else np.inf
 )
 
 dual_simplex_div = np.vectorize(
+    # Special division option used in dual algorithm.
     lambda n, d: -1 * n / d if d < 0 else np.inf
 )
 
