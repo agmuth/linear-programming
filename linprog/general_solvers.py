1b1a0ec4441d89fdfd86a19313e5cec97eacaa80
Andrew Muth <agmuth@gmail.com>
2023-07-13 13:36:26 -0600

    genearl solvers passing tests

--- linprog/general_solvers.py
+++ linprog/general_solvers.py
@@ -3,7 +3,7 @@ import numpy as np
 from linprog.primal_simplex_solvers import *
 
 
-class TwoPhaseSimplexSolver:
+class TwoPhaseTableauSimplexSolver:
     """
     Two Phase Simplex algorithm that implements Bland's selection rule to avoid cycling.
     """
@@ -71,8 +71,10 @@ class TwoPhaseSimplexSolver:
                     )
                     self.artificial_tableau.tableau.pivot(i, pivot_col)
 
-    def solve(self, maxiters=100):
+    def solve(self, maxiters=100, return_phase_1_basis: bool=False):
         self._drive_artificial_variables_out_of_basis(maxiters)
+        if return_phase_1_basis:
+            return self.artificial_tableau.basis
         # PHASE II --------------------------------------
         self.tableau = PrimalTableauSimplexSolver(
             c=self.c,
@@ -84,40 +86,53 @@ class TwoPhaseSimplexSolver:
         return self.tableau.solve(maxiters)
     
     
-class TwoPhaseSolver():
+class TwoPhaseRevisedSimplexSolver():
     def __init__(self, c: np.array, A: np.array, b: np.array) -> None:
         self.c, self.A, self.b = np.array(c), np.array(A), np.array(b)
         self.m, self.n = A.shape
         self.row_offset = 1
-        self.col_offset = 1
-        self.artificial_tableau = None
-        self.tableau = None         
+        self.col_offset = 1        
 
-    def _phase_one(self):
+    def _phase_one(self, maxiters: int=100):
         c = np.hstack([np.zeros(self.n), np.ones(self.m)])
         A = np.hstack([self.A, np.eye(self.m)])
         b = np.array(self.b)
         basis = np.arange(self.n, self.n+self.m)
-        solver = PrimalRevisedSimplexSolver(c, A, b, basis)
-        res = solver.solve()
-        res
+        solver = PrimalRevisedSimplexSolver(c,A, b, basis)
+        res = solver.solve(maxiters)
+        
+        if res.cost > 0:
+            raise ValueError("Problem is unfeasible.")
+        
+        if res.basis.max() >= self.n:
+            #a rtificial vars in basis
+            # remove 0 level artifical variables by pivoting
+            for i in range(self.n, self.n+self.m):
+                if i in res.basis:
+                    non_basic_non_artifical_vars = np.array([i for i in range(self.n) if i not in res.basis])
+                    potential_pivots = (solver.inv_basis_matrix @ solver.A)[np.argmax(res.basis == i), non_basic_non_artifical_vars] > 0
+                    if potential_pivots.any():
+                        col_in_A_to_enter_basis = non_basic_non_artifical_vars[np.argmax(potential_pivots)]
+                        col_in_basis_to_leave_basis = np.where(res.basis == i)[0][0]
+                        solver.pivot(col_in_basis_to_leave_basis, col_in_A_to_enter_basis)
+                        res = solver._get_solver_return_object()
+            
+        if res.basis.max() >= self.n:
+            non_redundent_equs_idx = (res.basis <= self.n)
+            self.A = self.A[non_redundent_equs_idx]
+            self.b = self.b[non_redundent_equs_idx]
+            self.m = non_redundent_equs_idx.sum()
+            res.basis = res.basis[non_redundent_equs_idx]
+                        
+        return res.basis
     
-    def _phase_two(self):
-        pass
+    def solve(self, maxiters1: int=100, maxiters2: int=100, return_phase_1_basis:bool=False):
+        phase_2_starting_basis = self._phase_one(maxiters1)
+        if return_phase_1_basis:
+            return phase_2_starting_basis
+        # phase 2
+        solver = PrimalRevisedSimplexSolver(self.c, self.A, self.b, phase_2_starting_basis)
+        res = solver.solve(maxiters2)
+        return res
     
-    def solve(self):
-        pass
-    
-if __name__ == "__main__":
-    c = np.array([-1, 2, -3, 0])
-    A = np.array(
-        [
-            [1, 1, 1, 0],
-            [-1, 1, 2, 0],
-            [0, 2, 3, 0],
-            [0, 0, 1, 1],
-        ]
-    )
-    b = np.array([6, 4, 10, 2])
-    solver = TwoPhaseSolver(c, A, b)
-    solver._phase_one()
+
--- tests/test_general_solvers.py
+++ tests/test_general_solvers.py
@@ -5,7 +5,7 @@ from linprog.general_solvers import *
 from tests.constants import TOL
 from tests.problems import PRIMAL_BASE_SOLVER_PROBLEMS
 
-SOLVERS = [TwoPhaseSimplexSolver]
+SOLVERS = [TwoPhaseTableauSimplexSolver, TwoPhaseRevisedSimplexSolver]
 
 
 @pytest.mark.parametrize("problem", PRIMAL_BASE_SOLVER_PROBLEMS)
@@ -14,3 +14,35 @@ def test_general_simplex_solver_for_correct_soln(problem, solver):
     solver = solver(problem.c, problem.A, problem.b)
     res = solver.solve()
     assert np.array_equal(res.basis, problem.optimal_basis)
+
+@pytest.mark.parametrize("solver", SOLVERS)
+def test_redundent_constraints(solver):
+    c = np.array([-1, 2, -3, 0])
+    A = np.array(
+        [
+            [1, 1, 1, 0],
+            [-1, 1, 2, 0],
+            [0, 2, 3, 0],
+            [0, 0, 1, 1],
+        ]
+    )
+    b = np.array([6, 4, 10, 2])
+    solver = solver(c, A, b)
+    res = solver.solve()
+    assert np.array_equal(res.x[res.basis], np.array([2, 2, 2]))
+    
+
+@pytest.mark.parametrize("solver", SOLVERS)
+def test_infeasible_constraints(solver):
+    c = np.array([-3, 4, 0, 0])
+    A = np.array(
+        [
+            [1, 1, 1, 0],
+            [2, 3, 0, -1],
+        ]
+    )
+    b = np.array([4, 18])
+    solver = solver(c, A, b)
+    with pytest.raises(Exception):
+        solver.solve()
+    
