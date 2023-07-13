e2a10a4a9a371c871dcc77dda58f4af5aa6062ae
Andrew Muth <agmuth@gmail.com>
2023-07-13 13:44:19 -0600

    misc comments

--- linprog/general_solvers.py
+++ linprog/general_solvers.py
@@ -8,7 +8,7 @@ class TwoPhaseTableauSimplexSolver:
     Two Phase Simplex algorithm that implements Bland's selection rule to avoid cycling.
     """
 
-    def __init__(self, c: np.array, A: np.array, b: np.array) -> None:
+    def __init__(self, c: np.array, A: np.array, b: np.array):
         """
         Assumes LP is passed in in standard form (min c'x sbj. Ax = b, x >= 0)
 
@@ -33,9 +33,12 @@ class TwoPhaseTableauSimplexSolver:
             basis=np.arange(self.n, self.n + self.m),
         )
 
-        self.artificial_tableau.solve(maxiters)
-        if self.artificial_tableau.tableau.tableau[0, 0] < 0:
-            raise ValueError("Problem does not have any feasible solutions.")
+        res = self.artificial_tableau.solve(maxiters)
+        if res.cost > 0:
+            if res.optimum:
+                raise ValueError("Problem is unfeasible.")
+            else:
+                raise ValueError("Phase one did not converge.")
 
         # drive any remaining aritificial variables out of basis
         for i in range(self.m + self.n - 1, self.n, -1):
@@ -87,13 +90,37 @@ class TwoPhaseTableauSimplexSolver:
     
     
 class TwoPhaseRevisedSimplexSolver():
-    def __init__(self, c: np.array, A: np.array, b: np.array) -> None:
+    def __init__(self, c: np.array, A: np.array, b: np.array):
+        """Assumes LP is passed in in standard form (min c'x sbj. Ax = b, x >= 0)
+
+        Args:
+            c (np.array): length n vector cost vector.
+            A (np.array): m by n matrix defining the linear combinations to be subject to equality constraints.
+            b (np.array): length m vector defining the equalies constraints.
+        """
         self.c, self.A, self.b = np.array(c), np.array(A), np.array(b)
         self.m, self.n = A.shape
         self.row_offset = 1
         self.col_offset = 1        
 
     def _phase_one(self, maxiters: int=100):
+        """Phase I of two phase simplex method.
+
+        Parameters
+        ----------
+        maxiters : int, optional
+            _description_, by default 100
+
+        Returns
+        -------
+        _type_
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
         c = np.hstack([np.zeros(self.n), np.ones(self.m)])
         A = np.hstack([self.A, np.eye(self.m)])
         b = np.array(self.b)
@@ -102,11 +129,14 @@ class TwoPhaseRevisedSimplexSolver():
         res = solver.solve(maxiters)
         
         if res.cost > 0:
-            raise ValueError("Problem is unfeasible.")
+            if res.optimum:
+                raise ValueError("Problem is unfeasible.")
+            else:
+                raise ValueError("Phase one did not converge.")
         
         if res.basis.max() >= self.n:
-            #a rtificial vars in basis
-            # remove 0 level artifical variables by pivoting
+            # artificial variables still in basis at zero level 
+            # pivot out when/where possible
             for i in range(self.n, self.n+self.m):
                 if i in res.basis:
                     non_basic_non_artifical_vars = np.array([i for i in range(self.n) if i not in res.basis])
@@ -118,6 +148,8 @@ class TwoPhaseRevisedSimplexSolver():
                         res = solver._get_solver_return_object()
             
         if res.basis.max() >= self.n:
+            # unable to pivot out one or more artifical vairables from basis
+            # constraints redundent -> remove
             non_redundent_equs_idx = (res.basis <= self.n)
             self.A = self.A[non_redundent_equs_idx]
             self.b = self.b[non_redundent_equs_idx]
@@ -127,6 +159,22 @@ class TwoPhaseRevisedSimplexSolver():
         return res.basis
     
     def solve(self, maxiters1: int=100, maxiters2: int=100, return_phase_1_basis:bool=False):
+        """Implement two phase simplex solver.
+
+        Parameters
+        ----------
+        maxiters1 : int, optional
+            Maximum number of iters in phase one, by default 100
+        maxiters2 : int, optional
+           Maximum number of iters in phase two, by default 100
+        return_phase_1_basis : bool, optional
+            Return basis from phase one no phase two, by default False
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
         phase_2_starting_basis = self._phase_one(maxiters1)
         if return_phase_1_basis:
             return phase_2_starting_basis
